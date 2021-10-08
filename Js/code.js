a285cb5c216f61bc27201e48a6fb3e709d9421b9
Dani <100007160.joan23@fje.edu>
2021-10-08 19:51:02 +0200

    Js

--- Js/code.js
+++ Js/code.js
@@ -1,3 +1,10 @@
 function calculo() {
-    alert("hellow");
+    //    alert("hellow");
+    let total_cuenta = document.getElementById('total').value;
+    let servicio = document.getElementById('servicio').value;
+    let personas = document.getElementById('personas').value;
+    /*alert(total_cuenta);
+    alert(servicio);
+    alert(personas);*/
+    console.log('total: ' + total_cuenta + ' servicio: ' + servicio + ' personas: ' + personas)
 }
