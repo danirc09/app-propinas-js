54169235226376c56cfedf51b637f529987d730d
Dani <100007160.joan23@fje.edu>
2021-10-11 19:41:28 +0200

    reset

--- Js/code.js
+++ Js/code.js
@@ -1,10 +1,22 @@
 function calculo() {
-    //    alert("hellow");
     let total_cuenta = document.getElementById('total').value;
     let servicio = document.getElementById('servicio').value;
     let personas = document.getElementById('personas').value;
-    /*alert(total_cuenta);
-    alert(servicio);
-    alert(personas);*/
-    console.log('total: ' + total_cuenta + ' servicio: ' + servicio + ' personas: ' + personas)
+    let res = (servicio * total_cuenta) / personas;
+    if (isNaN(res)) {
+        alert('Introduce datos validos');
+    } else if (personas == '' || personas == 0) {
+        alert('Introduce datos validos');
+    } else {
+        console.log('total: ' + total_cuenta + ' servicio: ' + servicio + ' personas: ' + personas)
+        console.log(res);
+        document.getElementById("res").innerHTML = "<p>Total a pagar</p><h3>" + res + " euros por persona</h3>";
+
+    }
+}
+
+function limpiarFormulario() {
+    document.getElementById('total').value = '';
+    document.getElementById('personas').value = '';
+    document.getElementById('res').innerHTML = '';
 }
